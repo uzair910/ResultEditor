bb372900ece07be1497237a8ec1faf899134fecb
Uzair <uzair.az@gmail.com>
2021-09-20 21:20:41 +0300

    first test case

---
+++ UnitTests/ParserController_Tests.cs
@@ -0,0 +1,36 @@
+﻿
+using Moq;
+using NUnit.Framework;
+using ResultStudio.Common;
+using ResultStudio.Controllers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UnitTests
+{
+    public class ParserController_Tests
+    {
+        private ParserController parserController;
+        private const string sIntialDirectory = "..\\..\\..\\TestData";
+
+        [SetUp]
+        public void Setup()
+        {
+            parserController = new ParserController();
+        }
+    
+        [TestCase("Input_Data1.txt", Description = "Test reading and parsing method with a valid file.")]
+        public void TestFileRead(string sFileName)
+        {
+            string message = string.Empty;
+            string sFullFilePath = Path.Combine(TestContext.CurrentContext.TestDirectory, sIntialDirectory, sFileName);
+            Dictionary<int, Vector> data;
+            parserController.ParseFile(sFullFilePath, out message, out data);
+
+            Assert.IsNotNull(data);
+            Assert.That(data.Count > 0);
+            Assert.IsFalse(message.ToUpper().Contains("ERROR"));
+        }
+    }
+}
--- UnitTests/TestClassTemplate.cs
+++ UnitTests/TestClassTemplate.cs
@@ -1,21 +1,29 @@
 ﻿
 using Moq;
 using NUnit.Framework;
-
+using ResultStudio.Common;
+using ResultStudio.Controllers;
+using System.Collections.Generic;
+using System.IO;
 using System;
 
 namespace UnitTests
 {
     public class TestClassTemplate
     {
-      [SetUp]
-      public void Setup()
-      {
-      }
+        private ParserController parserController;
+        private const string sIntialDirectory = "..\\..\\..\\TestData";
+        [SetUp]
+        public void Setup()
+        {
+            parserController = new ParserController();
+        }
 
-      [Test]
-      public void Test()
-      {
-      }
+        [Test]
+        public void Test()
+        {
+        }
+
+   
     }
 }
